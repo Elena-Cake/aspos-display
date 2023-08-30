586651a16c93d6810d3a742bedbcb10eabca0ba6
Elena Sveshnikova <lenamarinkevich@mail.ru>
2023-08-30 15:03:37 +0300

    add main page

--- src/App.tsx
+++ src/App.tsx
@@ -1,59 +1,48 @@
-import React, { useState } from 'react';
-import {
-  Routes, Route,
-} from 'react-router-dom';
+import React from 'react';
+import { Routes, Route, Link } from 'react-router-dom';
 import './App.scss';
 import Header from './components/Header/Header';
-import Button from './components/Button/Button';
 import FrameItem from './components/FrameItem';
+// url to all, path to frame
+export const links = [
+  {
+    title: 'Информация о функциональном состоянии ОЭС АСПОС ОКП и ОКН',
+    path: '#',
+    url: 'https://omcc.ru/'
+  },
+  {
+    title: 'Информация о результатах выполнения плана применения ОЭС АСПОС ОКП, ОКН и привлекаемых ОЭС',
+    path: 'https://plan.ksoes.ru/?&dark',
+    url: 'implementation-plan-results'
+  },
+  {
+    title: 'Информация о результатах обработки измерительной информации по КО в АЦУК-М',
+    path: '#',
+    url: 'measurement-information-processing'
+  },
+  {
+    title: 'Информация о вычислительных процессах и загрузки вычислительных средств',
+    path: '#',
+    url: 'computing'
+  },
+  {
+    title: 'Информация о взаимодействие АЦУК-М с ГИАЦ-М, ОЭС АСПОС ОКП, ОКН и привлекаемыми ОЭС',
+    path: '#s',
+    url: 'interaction'
+  }
+]
+type linksType = typeof links
 
 function App() {
-
-  const [activeLink, setActiveLink] = useState(null as null | number)
-
-  const links = [
-    {
-      title: 'Информация о функциональном состоянии ОЭС АСПОС ОКП и ОКН',
-      path: 'https://omcc.ru/',
-      url: 'functional-state'
-    },
-    {
-      title: 'Информация о результатах выполнения плана применения ОЭС АСПОС ОКП, ОКН и привлекаемых ОЭС',
-      path: 'https://omcc.ru/',
-      url: 'implementation-plan-results'
-    },
-    {
-      title: 'Информация о результатах обработки измерительной информации по КО в АЦУК-М',
-      path: 'https://omcc.ru/',
-      url: 'measurement-information-processing'
-    },
-    {
-      title: 'Информация о вычислительных процессах и загрузки вычислительных средств',
-      path: 'https://omcc.ru/',
-      url: 'computing'
-    },
-    {
-      title: 'Информация о взаимодействие АЦУК-М с ГИАЦ-М, ОЭС АСПОС ОКП, ОКН и привлекаемыми ОЭС',
-      path: 'https://omcc.ru/',
-      url: 'interaction'
-    }
-  ]
 
   return (
     <div className="App">
       <Header />
-      <div className="main__body">
-        <div className='menu'>
-          {
-            links.map((link, i) => {
-              const handleActiveLink = () => { setActiveLink(i) }
-              return <Button key={i} {...link} isActive={i === activeLink} setActiveLink={handleActiveLink} />
-            })
-          }
-        </div>
+      <div className="main">
         <Routes>
-          {links.map(link => <Route path={link.url} element={<FrameItem path={link.path} title={link.title} />} />)}
-          <Route path='*' element={<>Простите, такой страницы нет</>} />
+          <Route path='/' element={<Main />} />
+          <Route path={links[1].url} element={<FrameItem title={links[1].title} path={links[1].path} />} />
+          <Route path='*' element={<>Такой страницы не существует</>} />
         </Routes>
       </div>
     </div>
@@ -61,3 +50,14 @@ function App() {
 }
 
 export default App;
+
+const Main: React.FC<{}> = () => {
+
+  return <div className="menu">
+    <div className='buttons'>
+      {
+        links.map(link => <Link to={link.url} className={`link`} target="_blanck">{link.title} </Link>)
+      }
+    </div>
+  </div>
+}
